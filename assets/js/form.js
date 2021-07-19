ce75ebdfa4c8cb414c02c93397b190c3b5b9946f
komfiGolabi <komfalos1@gmail.com>
2021-07-19 21:05:07 +0000

    worked on the function to connect email to the page

---
+++ assets/js/form.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm){
+    emailjs.send("gmail", "explore-ethio" ,{
+        "from_name":contactForm.name.value , "from_email":contactForm.email.value,
+        "message_us":contactForm.message.value})
+    .then(
+        function(response) {
+            console.log("Your message has been sent. We get back to you shortly!" , response);
+        },
+        function(error){
+            console.log("Oops, something went wrong!! Try again!" , error);
+        }
+    );
+    return false;
+}
