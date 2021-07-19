dd7b6717bc4e5e7686daa43481977e708d1b4f0d
komfiGolabi <komfalos1@gmail.com>
2021-07-19 21:47:22 +0000

    added previoulsy deleted class to the inputs of my form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,14 @@
+
 function sendMail(contactForm){
-    emailjs.send("gmail", "explore-ethio" ,{
-        "from_name":contactForm.name.value , "from_email":contactForm.email.value,
+    emailjs.send("gmail", "explore-ethio",{
+        "from_name":contactForm.name.value , "from_email":contactForm.emailaddress.value,
         "message_us":contactForm.message.value})
     .then(
         function(response) {
             console.log("Your message has been sent. We get back to you shortly!" , response);
         },
         function(error){
-            console.log("Oops, something went wrong!! Try again!" , error);
+            console.log("Oops, something went wrong! Try again!", error);
         }
     );
     return false;
